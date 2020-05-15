1b9df61f46fe3c3f90f67c78d57182c4ee684c45
Anitha John <anithajohnpaul@gmail.com>
2020-05-15 11:32:43 +0530

    training

---
+++ server.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const express = require('express');
+var os = require('os');
+var fs = require('fs');
+const request = require('request');
+const dotenv = require('dotenv');
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+dotenv.config();
+var content = 'Namaste SocGen - Bangalore ' + process.env.USER + ' ' + os.hostname();
+
+app.get('/', (req, res) => {
+ 
+  res.send(content);
+});
+
+app.get('/savefile', (req,res) => {
+  fs.writeFile('mynewfile.txt', content, function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
+  });
+});
+
+app.get('/weather', (req, res) => {
+  console.log('The weather Service is at:' + process.env.WeatherService);
+
+  // fetch('http://localhost:32000/weatherforecast')
+  //   .then(res => res.json())
+  //   .then(json => { 
+  //     console.log(json);
+  //     content = json;
+  //   });
+
+  request('http://mintnodeapp-service.default.svc.cluster.local:32000/weatherforecast',(err,resp,body) => {
+    content = JSON.stringify(body);
+  });
+
+  res.send(content);
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
